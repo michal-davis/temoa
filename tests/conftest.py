1613eea75649bb96690ae13f0f55fb8487a85d5e
Jeff H <jeff@westernspark.us>
2023-10-09 13:20:31 -0700

    Cleaned up tests folder
    Tests running (except myopic, which is "on hold").  Added testing_outputs folder to catch database writes that occur during tests to preclude the input databases from being written to.  The output folder will automatically be populated with db copies if they aren't already there--and they should NOT be added to VCS

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import shutil
 
 from definitions import PROJECT_ROOT
 
@@ -9,6 +10,16 @@ from definitions import PROJECT_ROOT
 output_path = os.path.join(PROJECT_ROOT, "tests", "test_log")
 if not os.path.exists(output_path):
     os.mkdir(output_path)
+
+# ensure that dummy copies of utopia and test_system databases are available in testing_outputs
+# to catch data
+data_output_path = os.path.join(PROJECT_ROOT, 'tests', 'testing_outputs')
+data_source_path = os.path.join(PROJECT_ROOT, 'tests', 'testing_data')
+databases = 'temoa_utopia.sqlite', 'temoa_test_system.sqlite'
+for db in databases:
+    if not os.path.exists(os.path.join(data_output_path, db)):
+        shutil.copy(os.path.join(data_source_path, db), os.path.join(data_output_path, db))
+
 
 logging.getLogger("pyomo").setLevel(logging.INFO)
 filename = "testing.log"
---
+++ tests/testing_outputs/README.txt
@@ -0,0 +1,4 @@
+These are just "shunts" used as output targets for tests.
+
+Currently, they are never read and their entire purpose is to prevent (as much as possible) the tests from modifying
+the input databases during testing.
