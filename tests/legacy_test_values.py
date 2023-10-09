14a9256146dddc82fab1e10d3499b20f24b2cee6
Jeff H <jeff@westernspark.us>
2023-10-09 15:42:59 -0700

    Added test for number of vars and constraints
    added test for Utopia and Test System to verify the number of variables and constraints in each model vs. cached values

--- tests/legacy_test_values.py
+++ tests/legacy_test_values.py
@@ -13,14 +13,20 @@ class TestVals(Enum):
     OBJ_VALUE = 'obj_value'
     EFF_DOMAIN_SIZE = 'eff_domain_size'
     EFF_INDEX_SIZE = 'eff_index_size'
+    VAR_COUNT = 'count of variables in model'
+    CONSTR_COUNT = 'count of constraints in model'
 
 
 # these values were captured on base level runs of the .dat files in the tests/testing_data folder
 test_vals = {'config_test_system': {TestVals.OBJ_VALUE: 491977.7000753,
                                     TestVals.EFF_DOMAIN_SIZE: 30720,
-                                    TestVals.EFF_INDEX_SIZE: 74},
+                                    TestVals.EFF_INDEX_SIZE: 74,
+                                    TestVals.CONSTR_COUNT: 3066,
+                                    TestVals.VAR_COUNT: 1960},
              'config_utopia': {TestVals.OBJ_VALUE: 36535.631200,
                                TestVals.EFF_DOMAIN_SIZE: 12312,
-                               TestVals.EFF_INDEX_SIZE: 64},
+                               TestVals.EFF_INDEX_SIZE: 64,
+                               TestVals.CONSTR_COUNT: 1620,
+                               TestVals.VAR_COUNT: 1104},
              }
 
