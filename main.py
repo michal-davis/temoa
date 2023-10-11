c9683bb8b98bbee90e115cf0ea696663b7b8c527
Jeff H <jeff@westernspark.us>
2023-10-11 11:53:23 -0700

    Minor update:  Moved log creation to main.py
    Moved log/log folder creation to main.py, which should be a standard entry point.  Also, by screening to see if it is the named entry point, we can prevent tests from making unnecessary folders in the output files.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 """
 new entry point for running the model.
 """
+import logging
+import os
+from datetime import datetime
+
+from definitions import PROJECT_ROOT
 # Written by:  J. F. Hyink
 # jeff@westernspark.us
 # https://westernspark.us
@@ -31,4 +36,22 @@ def runModel():
         pass
 
 if __name__ == '__main__':
+    # set the target folder for logging output from this run
+    output_path = os.path.join(PROJECT_ROOT, "output_files", datetime.now().strftime("%Y-%m-%d %H%Mh"))
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+
+    # set up logger
+    logger = logging.getLogger(__name__)
+    logging.getLogger("pyomo").setLevel(logging.INFO)
+    logging.getLogger("matplotlib").setLevel(logging.WARNING)
+    filename = "log.log"
+    logging.basicConfig(
+        filename=os.path.join(output_path, filename),
+        filemode="w",
+        format="%(asctime)s | %(module)s | %(levelname)s | %(message)s",
+        datefmt="%d-%b-%y %H:%M:%S",
+        level=logging.DEBUG,  # <-- global change for project is here
+    )
+
     runModel()
--- temoa/temoa_model/temoa_model.py
+++ temoa/temoa_model/temoa_model.py
@@ -21,34 +21,8 @@ in LICENSE.txt.  Users uncompressing this from an archive may not have
 received this license file.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import logging
-import os
-from datetime import datetime
-
-from pyomo.core import AbstractModel, BuildAction
-from pyomo.environ import Set, Var, Param
-
-from definitions import PROJECT_ROOT
-from temoa.temoa_model.temoa_rules import *
 from temoa.temoa_model.temoa_initialize import *
-
-# set the target folder for logging output from this run
-output_path = os.path.join(PROJECT_ROOT, "output_files", datetime.now().strftime("%Y-%m-%d %H%Mh"))
-if not os.path.exists(output_path):
-    os.mkdir(output_path)
-
-# set up logger
-logger = logging.getLogger(__name__)
-logging.getLogger("pyomo").setLevel(logging.INFO)
-logging.getLogger("matplotlib").setLevel(logging.WARNING)
-filename = "log.log"
-logging.basicConfig(
-    filename=os.path.join(output_path, filename),
-    filemode="w",
-    format="%(asctime)s | %(module)s | %(levelname)s | %(message)s",
-    datefmt="%d-%b-%y %H:%M:%S",
-    level=logging.DEBUG,  # <-- global change for project is here
-)
+from temoa.temoa_model.temoa_rules import *
 
 
 class TemoaModel(AbstractModel):
