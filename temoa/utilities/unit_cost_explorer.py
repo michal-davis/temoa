af82bdf0edce827d7c6bcf01a8388bc80a56a2c3
Jeff H <jeff@westernspark.us>
2024-01-17 08:17:55 -0800

    Adding Some Basic Storage Tests
    These introduce Storageville, which has a battery for use in testing storage actions and some associated unit testing.  It also adds unit_cost_explorer.py which is a short little utility script to tease out the cost of 1 unit of storage from the model as a verification.

---
+++ temoa/utilities/unit_cost_explorer.py
@@ -0,0 +1,121 @@
+"""
+This file is intended as a QA tool for calculating costs associated with unit-sized purchases
+of storage capacity
+"""
+from temoa.temoa_model.temoa_initialize import *
+from temoa.temoa_model.temoa_rules import *
+
+# Written by:  J. F. Hyink
+# jeff@westernspark.us
+# https://westernspark.us
+# Created on:  12/30/23
+
+
+M = TemoaModel()
+
+"""
+let's fill in what we need to cost 1 item...
+The goal here is to cost out 1 unit of storage capacity in 1 battery in the year 2020
+in a generic region 'A'.
+
+This script is largely a verification of the true cost of 1 unit of storage because the math to
+calculate it is somewhat opaque due to the complexity of the cost function and the numerous
+factors that are used in calculation
+"""
+
+
+# indices
+rtv = ('A', 'battery', 2020)         # rtv
+rptv = ('A', 2020, 'battery', 2020)  # rptv
+
+# make SETS
+M.NewCapacityVar_rtv.construct(data=rtv)
+M.CapacityVar_rptv.construct(data=rptv)
+M.CostInvest_rtv.construct(data=rtv)
+M.CostFixed_rptv.construct(data=rptv)
+M.LifetimeLoanProcess_rtv.construct(data=rtv)
+M.Loan_rtv.construct(data=rtv)
+M.DiscountRate_rtv.construct(data=rtv)
+M.LifetimeProcess_rtv.construct(data=rtv)
+M.RegionalIndices.construct(data=['A'])
+M.MyopicBaseyear.construct(data={None:0})
+M.ModelProcessLife_rptv.construct(data=rptv)
+
+
+# make PARAMS
+M.time_optimize.construct([2020, 2025])
+M.time_future.construct([2020, 2025, 2030])  # needs to go 1 period beyond optimize horizon
+M.PeriodLength.construct()
+M.tech_all.construct(data=['battery'])
+M.regions.construct(data=['A'])
+M.CostInvest.construct(data={rtv: 1300})   # US_9R_8D
+M.CostFixed.construct(data={rptv: 20})     # US_9R_8D
+M.LifetimeLoanProcess.construct(data={rtv: 10})
+M.DiscountRate.construct(data={rtv: 0.05})
+M.LoanAnnualize.construct()
+M.LifetimeTech.construct(data={('A', 'battery'): 20})
+M.LifetimeProcess.construct(data={rtv: 40})
+M.ModelProcessLife.construct(data={rptv: 20})
+M.GlobalDiscountRate.construct(data={None: 0.05})
+
+# make/fix VARS
+M.V_NewCapacity.construct()
+M.V_NewCapacity[rtv].set_value(1)
+
+M.V_Capacity.construct()
+M.V_Capacity[rptv].set_value(1)
+
+# run the total cost rule on our "model":
+tot_cost_expr = TotalCost_rule(M)
+total_cost = value(tot_cost_expr)
+print()
+print(f'Total cost for building 1 capacity unit of storage:  ${total_cost:0.2f} [$M]')
+print('The total cost expression:')
+print(tot_cost_expr)
+
+# how much storage achieved for 1 unit of capacity?
+storage_cap = 1 # unit
+storage_dur = 4 # hr
+c2a         = 31.536 # PJ/GW-yr
+c           = 1/8760 #yr/hr
+storage = storage_cap * storage_dur * c2a * c
+PJ_to_kwh = 1/3600000 * 1e15
+print()
+print(f'storage built: {storage:0.4f} [PJ] / {(storage * PJ_to_kwh):0.2f} [kWh]')
+
+price_per_kwh = total_cost * 1e6/(storage * PJ_to_kwh)
+print(f'price_per_kwh: ${price_per_kwh: 0.2f}\n')
+
+# let's look at the constraint for storage level
+print('building storage level constraint...')
+
+# More SETS
+M.time_season.construct(data=['winter', 'summer'])
+tod_slices = 2
+M.time_of_day.construct(data=range(1, tod_slices + 1))
+M.tech_storage.construct(data=['battery'])
+M.ProcessLifeFrac_rptv.construct(data=[rptv])
+M.StorageLevel_rpsdtv.construct(data=[('A', 2020, 'winter', 1, 'battery', 2020),])
+M.StorageConstraints_rpsdtv.construct(data=[('A', 2020, 'winter', 1, 'battery', 2020),])
+
+# More PARAMS
+M.CapacityToActivity.construct(data={('A', 'battery'): 31.536})
+M.StorageDuration.construct(data={('A', 'battery'): 4})
+seasonal_fractions = {'winter': 0.4, 'summer': 0.6}
+M.SegFrac.construct(data={(s, d): seasonal_fractions[s] / tod_slices for d in M.time_of_day for s in M.time_season})
+# QA the total
+print(f'quality check.  Total of all SegFrac: {sum(M.SegFrac.values()):0.3f}')
+M.ProcessLifeFrac.construct(data={('A', 2020, 'battery', 2020): 1.0})
+
+# More VARS
+M.V_StorageLevel.construct()
+M.SegFracPerSeason.construct()
+
+upper_limit = StorageEnergyUpperBound_Constraint(M,  'A', 2020, 'winter', 1, 'battery', 2020)
+print('The storage level constraint for the single period in the "super day":\n',upper_limit)
+
+# cross-check the multiplier...
+mulitplier = storage_dur * M.SegFracPerSeason['winter'] * 365 * c2a * c
+print(f'The multiplier for the storage should be: {mulitplier}')
+
+M.StorageEnergyUpperBoundConstraint.construct()
---
+++ tests/test_storage.py
@@ -0,0 +1,161 @@
+"""
+The intent of this file is to test the storage relationships in the model
+
+Written by:  J. F. Hyink
+jeff@westernspark.us
+https://westernspark.us
+Created on:  12/29/23
+"""
+import logging
+import pathlib
+
+import pytest
+
+from definitions import PROJECT_ROOT
+from temoa.temoa_model.temoa_mode import TemoaMode
+from temoa.temoa_model.temoa_model import TemoaModel
+from temoa.temoa_model.temoa_sequencer import TemoaSequencer
+
+logger = logging.getLogger(__name__)
+# suitable scenarios for storage testing....singleton for now.
+storage_config_files = [
+    {'name': 'storageville', 'filename': 'config_storageville.toml'},
+    {'name': 'test_system', 'filename': 'config_test_system.toml'},
+]
+
+@pytest.mark.skip(reason="known not working...fix deferred")
+@pytest.mark.parametrize(
+    'system_test_run',
+    argvalues=storage_config_files,
+    indirect=True,
+    ids=[d['name'] for d in storage_config_files],
+)
+def test_initialization_in_last_period(system_test_run):
+    """
+    The last period should end up back at the initialization value
+    """
+    model: TemoaModel  # helps with typing for some reason...
+    data_name, results, model = system_test_run
+    assert len(model.V_StorageInit.index_set()) > 0, (
+        'This model does not appear to have' 'any available storage components'
+    )
+    # get references to last timeslot for each prtv combo in things with storage
+    last_slots = {
+        (r, p, s, d, t, v)
+        for r, p, s, d, t, v in model.StorageLevel_rpsdtv
+        if s == model.time_season.last() and d == model.time_of_day.last()
+    }
+    # test that the last day/season combo ends up at the initialization value
+    # devnote:  the Level variable is assessed at the end of the timeperiod
+    for r, p, s, d, t, v in last_slots:
+        assert model.V_StorageLevel[r, p, s, d, t, v].value == pytest.approx(
+            model.V_StorageInit[r, t, v].value, rel=1e-3
+        ), f'model fails to align last season/day slot with initialization value for {r, t, v}'
+
+
+@pytest.mark.parametrize(
+    'system_test_run',
+    argvalues=storage_config_files,
+    indirect=True,
+    ids=[d['name'] for d in storage_config_files],
+)
+def test_initialization_in_first_period(system_test_run):
+    """
+    The level at the end of the first period should be the initialization ± flows
+    """
+
+    model: TemoaModel  # helps with typing for some reason...
+    data_name, results, model = system_test_run
+    assert len(model.V_StorageInit.index_set()) > 0, (
+        'This model does not appear to have' 'any available storage components'
+    )
+    # test the first periods
+    # get references to first timeslot for each prtv combo in things with storage
+    first_slots = {
+        (r, p, s, d, t, v)
+        for r, p, s, d, t, v in model.StorageLevel_rpsdtv
+        if s == model.time_season.first() and d == model.time_of_day.first()
+    }
+    # test that the last day/season combo starts up at the initialization value
+    # devnote:  the Level variable is assessed at the end of the timeperiod, so the init ± flows
+    #           should total the end value
+    for r, p, s, d, t, v in first_slots:
+        inflow_indices = {
+            (rr, pp, ss, dd, ii, tt, vv, oo)
+            for rr, pp, ss, dd, ii, tt, vv, oo in model.FlowInStorage_rpsditvo
+            if all((rr == r, pp == p, ss == s, dd == d, tt == t, vv == v))
+        }
+        outflow_indices = {
+            (rr, pp, ss, dd, ii, tt, vv, oo)
+            for (rr, pp, ss, dd, ii, tt, vv, oo) in model.FlowVar_rpsditvo
+            if all((rr == r, pp == p, ss == s, dd == d, tt == t, vv == v))
+        }
+
+        # calculate the inflow and outflow.  Inflow is taxed by efficiency in the model,
+        # so we need to do that here as well
+        inflow = sum(
+            model.V_FlowIn[r, p, s, d, i, t, v, o].value * model.Efficiency[r, i, t, v, o]
+            for (r, p, s, d, i, t, v, o) in inflow_indices
+        )
+        outflow = sum(model.V_FlowOut[idx].value for idx in outflow_indices)
+        start = model.V_StorageInit[r, t, v].value
+        assert model.V_StorageInit[r, t, v].value + inflow - outflow == pytest.approx(
+            model.V_StorageLevel[r, p, s, d, t, v].value, rel=1e-3
+        ), f'model fails to align last season/day slot with initialization value for {r, t, v}'
+
+
+@pytest.mark.parametrize(
+    'system_test_run',
+    argvalues=storage_config_files,
+    indirect=True,
+    ids=[d['name'] for d in storage_config_files],
+)
+def test_storage_flow_balance(system_test_run):
+    """
+    Test the balance of all inflows vs. all outflows.
+    Note:  inflows are taxed by efficiency, so that is replicated here
+    """
+    model: TemoaModel  # helps with typing for some reason...
+    data_name, results, model = system_test_run
+    assert len(model.V_StorageInit.index_set()) > 0, (
+        'This model does not appear to have' 'any available storage components'
+    )
+    for s_tech in model.tech_storage:
+        inflow_indices = {
+            (r, p, s, d, i, t, v, o)
+            for r, p, s, d, i, t, v, o in model.FlowInStorage_rpsditvo
+            if t == s_tech
+        }
+        outflow_indices = {
+            (r, p, s, d, i, t, v, o)
+            for r, p, s, d, i, t, v, o in model.FlowVar_rpsditvo
+            if t == s_tech
+        }
+
+        # calculate the inflow and outflow.  Inflow is taxed by efficiency in the model,
+        # so we need to do that here as well
+        inflow = sum(
+            model.V_FlowIn[r, p, s, d, i, t, v, o].value * model.Efficiency[r, i, t, v, o]
+            for (r, p, s, d, i, t, v, o) in inflow_indices
+        )
+        outflow = sum(model.V_FlowOut[idx].value for idx in outflow_indices)
+        assert inflow == pytest.approx(
+            outflow, rel=1e-3
+        ), f'the inflow and outflow of storage tech {s_tech} do not match'
+
+@pytest.mark.skip('not ready for primetime')
+def test_hard_initialization():
+
+    filename = 'config_storageville.toml'
+    options = {'silent': True, 'debug': True}
+    config_file = pathlib.Path(PROJECT_ROOT, 'tests', 'testing_configs', filename)
+
+    sequencer = TemoaSequencer(
+        config_file=config_file,
+        output_path=tmp_path,
+        mode_override=TemoaMode.BUILD_ONLY,
+        **options,
+    )
+    # get a built, unsolved model
+    model = sequencer.start()
+    model.V_StorageInit['electricville', 'batt', 2025] = 0.5
